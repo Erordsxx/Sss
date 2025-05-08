593227e40e3b37af4a86a722d102a8c0c0722d81
Erordsxx <erikknewss@gmail.com>
2025-05-08 16:27:17 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,9 @@
+import telebot, random, time, requests 
+from telebot import types
 
+bot = telebot.TeleBot(TOKEN, parse_mode="HTML")
+TOKEN = ""
+
+@bot.message_handler(commands=["start"])
+def start_message(message):
+	bot.send_message(
